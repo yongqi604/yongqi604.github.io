106ffc12441d96962219ae1aa4a75b173d9f62c1
yongqi604 <373417325@qq.com>
2026-08-02 19:57:48 +0800

    Delete plugins/capacitor-printer directory

--- plugins/capacitor-printer/dist/plugin.d.ts
+++
@@ -1,3 +0,0 @@
-export interface PrinterPlugin {
-  print(options: { html: string; name?: string }): Promise<void>;
-}
--- plugins/capacitor-printer/dist/plugin.js
+++
@@ -1,9 +0,0 @@
-import { registerPlugin } from '@capacitor/core';
-
-export interface PrinterPlugin {
-  print(options: { html: string; name?: string }): Promise<void>;
-}
-
-const Printer = registerPlugin<PrinterPlugin>('Printer');
-
-export { Printer };
